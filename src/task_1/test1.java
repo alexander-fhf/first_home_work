9c3afcfa60204cd0e1b5cdf0731037bb2923aa2c
alexander <nesterovsasa0@gmail.com>
2022-06-08 18:51:41 +0300

    home work

---
+++ src/task_1/test1.java
@@ -0,0 +1,19 @@
+package task_1;
+
+public class test1{
+    public static void main(String[] args) {
+     String hi = "Hello ";
+     String world = "world!";
+     char newlane = '\n';
+     System.out.print(hi);
+     System.out.print(world);
+     System.out.print(newlane);
+        System.out.print(hi);
+        System.out.print(world);
+        System.out.print(newlane);
+           System.out.print(hi);
+           System.out.print(world);
+
+
+    }
+}
---
+++ src/task_1/test2.java
@@ -0,0 +1,14 @@
+package task_1;
+
+public class test2 {
+    public static void main(String[] args) {
+//        вес
+        int weight = 60;
+//        рост
+        int height = 180;
+
+        int result =  (height * height) / weight;
+        System.out.print(result);
+
+    }
+}
